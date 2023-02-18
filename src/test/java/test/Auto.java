6744b6a5c4afa97088d5f7a0aec9cac57866ad58
LibardoNavarro <josenavarrop05@gmail.com>
2023-02-18 12:30:43 -0500

    Taller 2 java
    Primer commit

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,41 @@
+package test;
+
+public class Auto {
+    public String modelo;
+    public int precio;
+    public Asiento[] asientos;
+    public String marca;
+    public Motor motor;
+    public int registro;
+    public int static cantidadCreados;
+
+    public int cantidadAsientos(){
+        objetosAsiento = 0;
+
+        for (int i = 0; i < asientos.length; i++){
+
+            if (asientos[i] != null){
+
+                objetosAsiento++;
+            }
+        }
+        
+        return objetosAsiento;
+    }
+
+    public String verificarIntegridad(){
+
+        if (this.motor.registro != this.registro){
+            return "Las piezas no son originales";
+        }
+
+        for (int i = 0; i < asientos.length; i++){
+            if (asientos[i] != null && asientos[i].registro != this.registro){
+                return "Las piezas no son originales";
+            }
+        }
+
+        return "Auto original";
+    }
+
+}
